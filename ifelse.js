b39c38d3eb5dbf6959a6f1afe74f283205fc596c
Ragunath Jayakumar <146608347+RagunathJayakumar@users.noreply.github.com>
2025-03-24 12:52:10 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,54 @@
+/* var color='green';
+if (color=='red'){
+
+    console.log("stop")
+}
+else if(color=="yellow")
+{
+    console.log("get ready")
+}
+else if (color=="green"){
+    console.log("go")
+} */
+
+/* var season="fall";
+
+if(season=="spring"){
+    console.log("enjoy the spring")
+
+}
+else if(season=="summer"){
+    console.log("do fun things ")
+
+}
+else if(season=="autumn"||"fall"){
+    console.log("admire the fall ")
+
+}
+else if(season=="winter"){
+    console.log("stay warm")
+
+} */
+
+/* var score=72;
+
+if(score<50){
+    console.log("you need to improve")
+}
+else if(50<score && score<70){
+    console.log("good job")
+}
+else if(score > 70){
+    console.log("excellent preformance")
+
+} */
+
+var a=10;
+
+if(a%2==0){
+    console.log("it is a even number")
+}
+
+else{
+    console.log("it is a odd number")
+}
---
+++ script.js
@@ -0,0 +1,95 @@
+/* var basket =['apple','banana','mango','orange']
+var nfruite='kova'
+basket.push(nfruite)
+basket.pop()
+console.log(basket)
+console.log(typeof(basket)) */
+
+// var a=   basket.includes("apple")?"yes":"no"
+// console.log(a)
+
+// var cen= "emc"=="EMC"?"yes both are equal":"they are different";
+// console.log(cen);
+
+// var rain = false;
+// var isit_rain= rain?"take umberla":"enjoy sunshine";
+// console.log(isit_rain);
+
+/* 
+var age =21;
+console.log(age);
+var favcolor="orange";
+
+console.log(favcolor);
+v ar rain=false;
+var isit_rain=rain?"take umberla":"enjoy sunshine";
+console.log(isit_rain);
+console.log("my age is "+age+" my favourite color is "+ favcolor +" "+ isit_rain)
+
+ */
+/*                                           */
+
+// var issunny=true;
+// var wathermessage=issunny?"it's a sunny day":"it's not sunny today";
+// console.log(wathermessage);
+
+/*var age =21;
+var friendage =22;
+var totalage =age+friendage;
+// console.log(totalage)
+var isodler=age<friendage?"he is older then me":"i am older person then him";
+// console.log(isodler);
+var fnumber=30;
+var doublenumber=2*fnumber;
+// console.log(doublenumber);
+
+
+console.log("the total age is "+totalage+" "+isodler+" "+"the doubled number is "+doublenumber);*/
+
+
+
+// if else conditions
+
+// var rain = true;
+// if (rain){
+//     console.log("take an umberla")
+// }
+
+// else{
+//     console.log("enjoy sunshine")
+// }
+
+
+/* var homewok = true;
+if(homewok){
+    console.log("greate job")
+}
+else{
+    console.log("done your homework brfore playing")
+} */
+
+
+/* var cookies = true;
+if (cookies){
+    console.log("would you like a cookies")
+}
+else{
+    console.log("time to make more cookies")
+} */
+
+/* var brush = true; */
+/* if (brush){
+    console.log("yes i brush my teeth")
+}
+else{
+    console.log("go to brush my teeth")
+} */
+/* 
+var birthday =false;
+if(birthday){
+    console.log("happy birthday , Enjoy your special day")
+}
+else{
+    console.log("have a good day")
+} */
+/**/ 
